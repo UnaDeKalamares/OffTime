e72f518f1661d5166215cdd85ea54a334414da1b
UnaDeKalamares <cperez.camarasa@gmail.com>
2026-03-06 20:49:06 +0100

    Separate TimerData and TimerUIState

---
+++ app/src/main/java/es/unadekalamares/offtime/data/TimerData.kt
@@ -0,0 +1,6 @@
+package es.unadekalamares.offtime.data
+
+data class TimerData (
+    val topTimer: String = "00:00:00",
+    val bottomTimer: String = "00:00:00"
+)
--- app/src/main/java/es/unadekalamares/offtime/ui/timer/TimerUIState.kt
+++ app/src/main/java/es/unadekalamares/offtime/ui/timer/TimerUIState.kt
@@ -1,6 +1,5 @@
 package es.unadekalamares.offtime.ui.timer
 
-data class TimerUIState(
-    val topTimer: String = "00:00:00",
-    val bottomTimer: String = "00:00:00"
-)
+enum class TimerUIState {
+    Running, Stopped
+}
