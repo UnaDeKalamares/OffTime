9c5b5cf722ebad5a43530eae45ad5dca4acfdcb5
UnaDeKalamares <cperez.camarasa@gmail.com>
2026-03-07 10:38:30 +0100

    Handle notification tap

--- app/src/main/java/es/unadekalamares/offtime/notification/NotificationsHelper.kt
+++ app/src/main/java/es/unadekalamares/offtime/notification/NotificationsHelper.kt
@@ -3,12 +3,15 @@ package es.unadekalamares.offtime.notification
 import android.Manifest
 import android.app.Notification
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import es.unadekalamares.offtime.R
+import es.unadekalamares.offtime.ui.timer.TimerActivity
 import org.koin.java.KoinJavaComponent.inject
 
 object NotificationsHelper {
@@ -27,18 +30,22 @@ object NotificationsHelper {
         notificationManager.createNotificationChannelsCompat(listOf(channel))
     }
 
-    fun buildNotification(text: String, context: Context): Notification =
-        NotificationCompat.Builder(context, TIMER_NOTIFICATION_CHANNEL_ID)
-            .setContentTitle(
-                context.getString(R.string.timer_notification_title)
-            )
-            .setContentText(
-                text
-            )
+    fun buildNotification(text: String, context: Context): Notification {
+        val offTimeIntent = Intent(context, TimerActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+        val openIntent = PendingIntent.getActivity(context, 0, offTimeIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        return NotificationCompat.Builder(context, TIMER_NOTIFICATION_CHANNEL_ID)
+            .setContentTitle(context.getString(R.string.timer_notification_title))
+            .setContentText(text)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
             .setOnlyAlertOnce(true)
+            .setContentIntent(openIntent)
+            .setAutoCancel(true)
             .build()
+    }
 
     @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
     fun updateNotification(text: String, context: Context) {
